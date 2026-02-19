05afca31d592a170aebb1c4adbac544beb27618d
anshsingh <singhanshak57@gmail.com>
2026-02-19 19:47:05 +0530

    Day 7 tasks

---
+++ Recursion/recursion11.cpp
@@ -0,0 +1,17 @@
+//Check if a given string is pallindrome or not ....>>>>>>>
+#include <iostream>
+#include<string.h>
+using namespace std ;
+bool f(int i , string &s ){
+    if(i>=s.size()/2) return true ;
+    if(s[i]!=s[s.size()-i-1])
+    return false ;
+    return f(i+1 , s);
+}
+int main() {
+    string s ;
+    cout << "Input the string .";
+    cin >> s ;
+    cout << f(0,s) ;
+   return 0 ;
+}
