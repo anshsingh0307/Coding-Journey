15a871c4af986e9904dae9dfe3cd6ecb9c71f6bb
anshsingh <singhanshak57@gmail.com>
2026-03-29 20:39:22 +0530

    Leetcode

--- Leetcode Extras/PowerOfThree.cpp
+++ Leetcode Extras/PowerOfThree.cpp
@@ -1,16 +1,16 @@
-//Question - 231
+//Question - 326
 #include <bits/stdc++.h>
 using namespace std ;
 bool powerofTwo(int n){
             for(int i=0 ;i<=31 ; i++){
-        if((int)pow(2,i)==n)
+        if((int)pow(3,i)==n)
         return true ;
         }
         return false ;
 }
 
 int main() {
-    int n= 64 ;
+    int n= 81 ;
    cout << powerofTwo(n);
     return 0 ;
 }
