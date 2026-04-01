9a4ae31cf28b630d9d28ebd96a4b5459c393b4bc
anshsingh <singhanshak57@gmail.com>
2026-04-01 23:43:28 +0530

    Arrays

--- Arrays(Extra Questions)/Question4.cpp
+++ Arrays(Extra Questions)/Question4.cpp
@@ -2,8 +2,23 @@
 #include<bits/stdc++.h>
 using namespace std ;
      double findMaxAverage(vector<int>& nums, int k) {
-        
-    }
+        int sum=0 ;
+        for(int i=0 ; i<k ;i++){
+            sum+=nums[i];
+        }
+            int maxSum=sum ;
+            int startIndex=0;
+            int endIndex=k ;
+            while(endIndex<nums.size()){
+                sum-=nums[startIndex];
+                startIndex++;
+                sum+=nums[endIndex];
+                endIndex++;
+                maxSum=max(maxSum,sum);
+            }
+            return (double)maxSum/k ;
+        }
+
     int main(){
         vector<int>nums={1,12,-5,-6,50,3};
         cout<<findMaxAverage(nums,4);
