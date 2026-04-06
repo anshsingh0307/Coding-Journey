4d14e85754f1741a5a15b80eff7116884b21e386
anshsingh <singhanshak57@gmail.com>
2026-04-06 19:23:41 +0530

    Leetcode

---
+++ Leetcode Extras/AddDigits.cpp
@@ -0,0 +1,16 @@
+//Leetcode----->>>>>>>>258
+#include<bits/stdc++.h>
+using namespace std ;
+    int addDigits(int num) {
+        while(num >= 10) {   
+            int sum = 0;
+
+            while(num > 0) {
+                sum += num % 10;   
+                num /= 10;         
+            }
+
+            num = sum;   
+        }
+        return num;
+    }
