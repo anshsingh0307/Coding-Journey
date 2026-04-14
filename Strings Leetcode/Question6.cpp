8d6c0aa45ea77763bc4756baf228fec1de042088
anshsingh <singhanshak57@gmail.com>
2026-04-14 17:06:29 +0530

    Strings

---
+++ Strings Leetcode/Question6.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std ;
+string nextTerm( string prev) {
+    string curr = "";
+    int count = 1;
+    
+    for (int i = 1; i < prev.length(); i++) {
+        if (prev[i] == prev[i - 1]) {
+            count++;
+        } else {
+            curr += to_string(count) + prev[i - 1];
+            count = 1;
+        }
+    }
+
+    curr += to_string(count) + prev[prev.length() - 1];
+    
+    return curr;
+}
+    string countAndSay(int n) {
+         if (n == 1) {
+        return "1";
+    }
+
+    string prev = countAndSay(n - 1);
+
+    return nextTerm(prev);
+    }
