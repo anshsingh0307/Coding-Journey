95dc5a7eccbaf6958ff1c6a321a021c6d73aec65
anshsingh <singhanshak57@gmail.com>
2026-04-19 22:52:28 +0530

    Leetcode

---
+++ Binary Search/Question5.cpp
@@ -0,0 +1,19 @@
+//Find Smallest Letter Greater Than Target
+#include<bits/stdc++.h>
+using namespace std ;
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0;
+        int right = letters.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (letters[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return letters[left % letters.size()];
+    }
