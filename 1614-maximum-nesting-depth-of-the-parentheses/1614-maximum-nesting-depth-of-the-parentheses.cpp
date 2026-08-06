1eabf806cc12814df6f7038ec0df81b8546d4de7
Ansh Singh <singhanshak57@gmail.com>
2026-08-06 18:35:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.84%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st ;
         int n = s.length();
         int count = 0 ;
         int newCount= 0 ;
 
         for(int i=0 ; i<n ; i++){
             if(s[i]=='('){
-            st.push(s[i]) ;
             count++;
             }
             
             else if(s[i]==')'){
-                st.pop();
                 count-- ;
             }
             newCount = max(count , newCount );
